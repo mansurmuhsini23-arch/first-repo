580ca20db0aeb7642936437b47d0baab85cc6791
mansurmuhsini23-arch <mansurmuhsini23@gmail.com>
2026-03-06 22:47:06 +0800

    Add files via upload
    This program checks for prime numbers between any two numbers you enter. It runs through each number, tests if it's prime by seeing if anything divides into it evenly, and prints out the ones that pass the test. The output shows all primes found with nice formatting, plus tells you how many there are and what they add up to. Simple but useful for learning about loops and math functions in C++.

---
+++ primenum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+    bool isPrime(int x){
+        if(x<=0) return false;
+        for(int i=2;i*i<=x;i++){
+            if(x%i==0) return false;
+        }
+        return true;
+    }
+int main(){
+    int from,to,count=0,sum=0;;
+    cin>>from>>to;
+    for(int i=from;i<=to;i++){
+        if(isPrime(i)){
+            sum+=i;
+            count++;
+            cout<<setw(5)<<i<<" ";
+        
+
+        }
+        
+    }
+   cout<<endl;
+   cout<<"quantity of prime numbers are "<<count<<endl;
+   cout<<"sum of prime numbers are "<<sum<<endl;
+    return 0;
+    
+}
